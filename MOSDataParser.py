751342214ed784ad6b2a657a20b3d47da5021bc1
Jacob Hinson <91631352+kikoeta@users.noreply.github.com>
2021-09-29 15:18:17 -0400

    Uploaded corrected versions of both files

--- MOSDataParser.py
+++ MOSDataParser.py
@@ -1,36 +1,36 @@
 import pandas as pd
 
 # --- Comment out line below if testing, else will recheck model ---
-import WxWebScraperV3
+import MOSWebScraper
 
 # ---- GFS ----
 # Header with station, model, and run
-HEADER = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="B", header=1, nrows=0)
+HEADER = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="B", header=1, nrows=0)
 HEADER = HEADER.columns.values[0]
 print(HEADER)
 
 # Date
-DATE = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="B", header=2, nrows=0)
+DATE = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="B", header=2, nrows=0)
 DATE = DATE.columns.values[0]
 print(DATE)
 
 # Hour (UTC)
-HR = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="B", header=3, nrows=0)
+HR = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="B", header=3, nrows=0)
 HR = HR.columns.values[0]
 print(HR)
 
 # Max/Min Temp
-NX = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="B", header=4, nrows=0)
+NX = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="B", header=4, nrows=0)
 NX = NX.columns.values[0]
 print(NX)
 
 # Hourly Temp
-TMP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="B", header=5, nrows=0)
+TMP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="B", header=5, nrows=0)
 TMP = TMP.columns.values[0]
 print(TMP)
 
 # Hourly Windspeed
-WSP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="B", header=9, nrows=0)
+WSP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="B", header=9, nrows=0)
 WSP = WSP.columns.values[0]
 print(WSP)
 
@@ -38,49 +38,31 @@ print("\n")
 
 # ---- NAM ----
 # Header with station, model, and run
-HEADER = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="C", header=1, nrows=0)
+HEADER = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="C", header=1, nrows=0)
 HEADER = HEADER.columns.values[0]
 print(HEADER)
 
 # Date
-DATE = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="C", header=2, nrows=0)
+DATE = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="C", header=2, nrows=0)
 DATE = DATE.columns.values[0]
 print(DATE)
 
 # Hour (UTC)
-HR = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="C", header=3, nrows=0)
+HR = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="C", header=3, nrows=0)
 HR = HR.columns.values[0]
 print(HR)
 
 # Max/Min Temp
-NX = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="C", header=4, nrows=0)
+NX = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="C", header=4, nrows=0)
 NX = NX.columns.values[0]
 print(NX)
 
 # Hourly Temp
-TMP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="C", header=5, nrows=0)
+TMP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="C", header=5, nrows=0)
 TMP = TMP.columns.values[0]
 print(TMP)
 
 # Hourly Windspeed
-WSP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxWeb MOS Scraper.xlsx', index_col=None, usecols="C", header=9, nrows=0)
+WSP = pd.read_excel (r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/MOSScraperData.xlsx', index_col=None, usecols="C", header=9, nrows=0)
 WSP = WSP.columns.values[0]
 print(WSP)
-#print("\n")
-#TMP_Split = TMP.split() # Splits the unregulated text into readable bits
-#TMP_Split.remove("TMP") # Removes letters from data
-#NX_Split = NX.split()
-#NX_Split.remove("X/N")
-#if min(NX_Split) <= min(TMP_Split):
-#    print("Min temp is: ", min(NX_Split))
-#elif min(TMP_Split) <= min(NX_Split):
-#    print("Min temp is: ", min(TMP_Split))
-##if max(NX_Split) >= max(TMP_Split):
-#    print("Max temp is: ", max(NX_Split))
-#elif max(TMP_Split) >= max(NX_Split):
-#    print("Max temp is: ", max(TMP_Split))
-#
-#print("NX min is:", min(NX_Split))
-#print("NX max is:", max(NX_Split))
-#print("TMP min is:", min(TMP_Split))
-#print("TMP max is:", max(TMP_Split))
--- MOSWebScraper.py
+++ MOSWebScraper.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import openpyxl
 import xlrd
+
 # Web scraper to get data from GFS and NAM MOS bulletin forecast products and export into Excel file
 
+# --- WEBDRIVER PATH ---
 driver = webdriver.Chrome(executable_path='C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/chromedriver.exe')
+
+# --- LINK ---
 driver.get('https://www.weather.gov/mdl/mos_getbull?ele=mav,met&sta=kuil') # MAV and MET products for station ____, need to get for other times as well
 
 results = []
@@ -16,16 +20,9 @@ driver.quit()
 for element in soup.findAll('div', id='demo'):
     pre = element.find("pre")
     fullData = pre.find_next_siblings("pre", limit=3)
-    #preStage = pre.find_next_sibling("pre")
-    #NAM = preStage.find_next_sibling("pre").text
-    #splitGFS = GFS.splitlines()
-    #splitNAM = NAM.splitlines()
-#'NAM MOS Latest Data': splitNAM
 df = pd.DataFrame({'Lastest Data': fullData})
 df.to_excel('WxDataHold.xlsx')
 
-#Basically find a way to manipulate the placement of the data on the sheet and we're golden
-#All we need to do now is just grab the other table
 GFSRaw = pd.read_excel(r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data/WxDataHold.xlsx', index_col=None, usecols="B", header=1, nrows=0)
 GFSRaw = GFSRaw.columns.values[0]
 GFSData = GFSRaw.splitlines()
@@ -33,4 +30,4 @@ NAMRaw = pd.read_excel(r'C:/Users/Jacob/PycharmProjects/Web Scraper for Met Data
 NAMRaw = NAMRaw.columns.values[0]
 NAMData = NAMRaw.splitlines()
 df = pd.DataFrame({'GFS': GFSData, 'NAM': NAMData})
-df.to_excel('WxWeb MOS Scraper.xlsx', "Data")
+df.to_excel('MOSScraperData.xlsx', "Data")
